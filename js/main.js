3eb4381e31b101f659889f2084131455dcea5588
HUMB3-dev <humb3rt1co@gmail.com>
2021-05-22 17:50:42 -0800

    cambios con normalze css

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 document.addEventListener('DOMContentLoaded',()=>{
 const elementosCarousel= document.querySelectorAll('.carousel');
-M.Carousel.init(elementosCarousel {
-duration:150
-
-})
-
+M.Carousel.init(elementosCarousel ,{
+    duration:150,
+    
+    indicators:true,
+   
+} 
+    
+)
 });
- 
+var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
