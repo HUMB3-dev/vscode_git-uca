77ecf4af36e3c56c7ab077d2f436b42d9dd1dce7
HUMB3-dev <humb3rt1co@gmail.com>
2021-05-23 17:46:11 -0800

    nuevo look

--- js/main.js
+++ js/main.js
@@ -9,3 +9,17 @@ indicators:true,
     
 )
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, );
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems,);
+  });
+
+  // Or with jQuery
+
+  $('.dropdown-trigger').dropdown();
